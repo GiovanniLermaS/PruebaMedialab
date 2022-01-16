6c042c42a4e47d25693b69e99bf4d461d7a6d5ae
Giovanni Lerma Suarez <giosuarezler@gmail.com>
2022-01-16 13:29:36 -0500

    View binding added.

--- app/src/main/java/com/example/pruebamedialab/view/main/MainActivity.kt
+++ app/src/main/java/com/example/pruebamedialab/view/main/MainActivity.kt
@@ -3,10 +3,19 @@ package com.example.pruebamedialab.view.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.pruebamedialab.R
+import com.example.pruebamedialab.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
     }
 }
